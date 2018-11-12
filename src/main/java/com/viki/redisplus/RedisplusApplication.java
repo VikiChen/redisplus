9be2f8014db81e02e34b9326cf79dbae4687f222
Viki Chen <viki@chinadep.com>
2018-11-12 12:48:27 +0800

    1.起航

---
+++ src/main/java/com/viki/redisplus/RedisplusApplication.java
@@ -0,0 +1,12 @@
+package com.viki.redisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisplusApplication.class, args);
+    }
+}
---
+++ src/test/java/com/viki/redisplus/RedisplusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.viki.redisplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisplusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
